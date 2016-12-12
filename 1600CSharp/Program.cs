34e0341606e272312e120edc3e4ba3b4f582fb84
mbtc <matthew.btc@gmail.com>
2016-12-12 15:49:55 -0700

    Updated classes

--- 1600CSharp/Program.cs
+++ 1600CSharp/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 public class Program
 {
-
-    public static Game myGame = new Game();
-    public static void Main(string[] args)
+    public static void Main (string[]) args)
     {
-        Console.WriteLine("Please type in your name:");
-        myGame.name = Console.ReadLine();
-        Console.WriteLine("Your Player Name is " + mygame.name);
-        myGame.Start();
+        Console.WriteLine ("If you are ready to start your adventure, select a class...");
+        Console.WriteLine ("Warrior (sword and shield fighter), Mage (a spell caster), Lancer (A spear artsman).");
+        CharacterClass = Console.ReadLine ().ToUpper ();
+        if (class== "Warrior"){
+        }if (class== "Mage"){
+        }if (class== "Lancer"){
+        } else{
+        }
+
     }
 }
