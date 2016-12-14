20a38176ccfb19ebe9dfde8cecebbd485f4ebf7b
mbtc <matthew.btc@gmail.com>
2016-12-14 11:10:22 -0700

    added list

---
+++ 1600CSharp/GameScripts/Inventory.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Inventory.Generic;
+
+public class Inventory
+public static void Main()
+{
+List<string> items = new List<string>();
+items.Add("Potion");
+items.Add("Spell Powder");
+items.Add("Run Potion");
+items.Add("Sword");
+items.Add("Staff");
+items.Add("Spear");
+
+}
