10c7163c74e903e67d4ff798c7dfe41e642e6e58
mbtc <matthew.btc@gmail.com>
2016-12-13 20:11:07 -0700

    yup

--- 1600CSharp/Characters/Enemies.cs
+++ 1600CSharp/Characters/Enemies.cs
@@ -1,5 +1,5 @@
 using System;
-public class Enemies
+public class Enemies 
 
 public string[] Enemy = {"Swoop Bat", "Faned Beast", "Soft Troll", "Sour Mage", "Damped Dragon"};
 Random Enemy = new Random ();
@@ -8,4 +8,16 @@ Random Enemy = new Random ();
 if (Random)
 {
     Enemy = {"Swoop Bat", "Faned Beast", "Soft Troll", "Sour Mage", "Damped Dragon"};
+}
+
+
+Attack menue{"Attack", "Defend", "Special", "Run"}
+
+class Warrior
+{
+Classes SwoopBat = new Classes(30, 5, 90);
+Classes FanedBeast = new Classes(90, 7, 100);
+Classes SoftTroll = new Classes(90, 10, 100);
+Classes SourMage = new Classes(100, 15, 80);
+Classes DampedDragon = new Classes(40, 15, 0);
 }
